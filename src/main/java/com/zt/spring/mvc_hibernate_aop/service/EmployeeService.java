4a3721f1a067f447fd78339fec8d18f556d04bfb
SolomonVP <Lum7023@yandex.ru>
2022-11-26 18:52:18 +0300

    add Service

---
+++ src/main/java/com/zt/spring/mvc_hibernate_aop/service/EmployeeService.java
@@ -0,0 +1,9 @@
+package com.zt.spring.mvc_hibernate_aop.service;
+
+import com.zt.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+    public List<Employee> getAllEmployees();
+}
